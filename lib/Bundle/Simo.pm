0b0b50c0ea65c24880fdda44e92259a86c4cea10
Yuki Kimoto <kimoto.yuki@gmail.com>
2009-03-12 07:04:48 +0000

    Import of KIMOTO/Bundle-Simo-0.0101 from CPAN.
    gitpan-cpan-distribution: Bundle-Simo
    gitpan-cpan-version:      0.0101
    gitpan-cpan-path:         KIMOTO/Bundle-Simo-0.0101.tar.gz
    gitpan-cpan-author:       KIMOTO
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Simo.pm
@@ -0,0 +1,35 @@
+package Bundle::Simo;
+use strict;
+use warnings;
+
+$VERSION = '0.0101';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Simo - A bundle to install L<Simo> related modules
+
+=head1 SYNOPSIS
+
+cpan Bundle::Simo
+
+=head1 CONTENTS
+
+Simo - Very simple framework for Object Oriented Perl
+
+Simo::Util - Utility Class for Simo
+
+Simo::Wrapper - Wrapper class to manipulate a object
+
+Simo::Error - Error object for Simo 
+
+Simo::Constrain - Constrain functions for Simo
+
+=head1 AUTHOR
+
+Yuki Kimoto, C<< <kimoto.yuki at gmail.com> >>
+
+=cut 
